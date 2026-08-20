02dd84dd5b95de9f6846dcf7593e78f6b222cc4b
harshitjoshi387 <harshjoshi25022002@gmail.com>
2026-08-20 19:28:23 +0530

    Add Header component and update App to use it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './Components/Header'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <Header name="harsh"/>
     </div>
   )
 }
