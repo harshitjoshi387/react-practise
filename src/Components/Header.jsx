fd2d7945bf070f1a12dc8f193e6b49c1ba9346c4
harshitjoshi387 <harshjoshi25022002@gmail.com>
2026-08-20 20:04:54 +0530

    Add button to Header component with click alert functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const Header = (props) => {
+    function click(){
+        alert("button clicked")
+    }
     
   return (
     <div>
       <h1>hello from {props.name}</h1>
+      <button onClick={click}>Click me</button>
     </div>
   )
 }
