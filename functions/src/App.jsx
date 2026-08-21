07747f6c168b9914e75845e46e27858e30926e06
harshitjoshi387 <harshjoshi25022002@gmail.com>
2026-08-21 22:43:18 +0530

    feat: add form and input field for user name entry

--- functions/src/App.jsx
+++ functions/src/App.jsx
@@ -5,8 +5,17 @@ const App = () => {
   function buttonClick(){
     alert("button is clicked")
   }
+  // function change(e){
+  //   console.log(e.target.value)
+  // }
   return (
     <div>
+      <form action="">
+        <input onChange={function(e){
+          console.log(e.target.value);
+          
+        }} type="text" placeholder='enter your name ' />
+      </form>
       <button onClick={buttonClick}>click me </button>
     </div>
   )
