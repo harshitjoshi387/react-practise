e99faa624359656ae93e1fa9cc26645f94f778bf
harshitjoshi387 <harshjoshi25022002@gmail.com>
2026-08-22 10:06:56 +0530

    preventDefault behaviour of forms in react js

---
+++ formhandling/src/Components/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="email" required />
+        <input type="password" placeholder="password" required />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
