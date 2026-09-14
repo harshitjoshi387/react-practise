358e8e9055e7443c765aaa1e65834fb22a9ebb38
harshitjoshi387 <harshjoshi25022002@gmail.com>
2026-09-14 22:19:18 +0530

    react form hook

--- formhandling/src/App.jsx
+++ formhandling/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import Login from './Components/Login'
+
+import { useForm } from "react-hook-form"
 
 const App = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+  const onSubmit =(data)=>console.log(data)
   return (
-    <div>
-      <Login />
-    </div>
+   <form onSubmit={handleSubmit(onSubmit)}>
+    <input {...register("email",{required:true})} />
+    <input {...register("password",{required:true,minLength:6})} 
+    
+    />
+    {errors.password && <p>password kam se kam 6 character</p>}
+    <button type="submit">submit</button>
+   </form>
   )
 }
 
